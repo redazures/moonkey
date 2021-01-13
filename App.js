0ad4cf92ffabc8270acecde15a353f956ccbba4c
James Wu <jwu_nyc@yahoo.com>
2021-01-13 15:37:44 -0500

    completed!

--- App.js
+++ App.js
@@ -12,16 +12,36 @@ import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions, 
 
 import SafeScreen from './app/components/SafeScreen'
 
+import { ListItem, Avatar } from 'react-native-elements'
+
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  },
+]
 
 const App: () => React$Node = () => {
   return (
-    <>
-    <SafeScreen>
-        <Text>
-          can this be here
-        </Text>
-    </SafeScreen>
-    </>
+    <View>
+      {
+        list.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+            <Avatar source={{uri: l.avatar_url}} />
+            <ListItem.Content>
+              <ListItem.Title>{l.name}</ListItem.Title>
+              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        ))
+      }
+    </View>
   );
 };
 
